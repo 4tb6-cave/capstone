d11f09145369edbe0c4d83716eb03e5476f7023d
x8man <abdulrahim4404@gmail.com>
2025-11-27 10:52:30 -0500

    Add\Update ICP algorithm files
    Added normals to the points in the Point Cloud. ICP runs on multiple
    point cloud files.

--- src/PointCloud2/src/icp_point_cloud.cpp
+++ src/PointCloud2/src/icp_point_cloud.cpp
@@ -1,52 +1,136 @@
-// How to run: ./<executable> /path/to/source.ply /path/to/target.ply
+// How to run: <executable> path/to/point-cloud/dir
 
+//#include <typeinfo>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <filesystem>
+#include <vector>
+#include <algorithm>
 #include <pcl/io/ply_io.h>
 #include <pcl/types.h>
 #include <pcl/registration/icp.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/console/print.h>
+
+
+namespace fs = std::filesystem;
+
+template <typename PointT>
+typename pcl::PointCloud<PointT>::Ptr voxelDownsample(
+    typename pcl::PointCloud<PointT>::Ptr inputCloud,
+    float leafSize) {
+
+        typename pcl::PointCloud<PointT>::Ptr filteredCloud (new pcl::PointCloud<PointT>());
+        typename pcl::VoxelGrid<PointT> voxel;
+        voxel.setInputCloud(inputCloud);
+        voxel.setLeafSize(leafSize, leafSize, leafSize);
+        voxel.filter(*filteredCloud);
+        return filteredCloud;
+    }
+
 
 int main (int argc, char** argv) {
 
-    // Check for correct number of arguments
+    pcl::console::setVerbosityLevel(pcl::console::L_ERROR);
+    
     if (argc < 3) {
-        throw std::runtime_error("Arguments missing!\n");
+        std::cerr << ("Arguments missing!\n");
         return 1;
     }
-
-    std::string source_PLY_file = argv[1];
-    std::string target_PLY_file = argv[2];
-
-    // Initialize point clouds
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_s (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_t (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr final_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-
-    // Load .ply from command line argument to point cloud variable 
-    pcl::io::loadPLYFile(source_PLY_file, *cloud_s);
-    pcl::io::loadPLYFile(target_PLY_file, *cloud_t);
-
-    // Run simple ICP algorithm for two point clouds
-    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-    icp.setInputSource(cloud_s);
-    icp.setInputTarget(cloud_t);
-
-    // Set criteria for ICP termination
-    icp.setTransformationEpsilon(1e-8);
-    //icp.setMaximumIterations(10);
-
-    // Store new aligned point cloud
-    icp.align(*final_cloud);
-
-    std::cout << "ICP has " << (icp.hasConverged()?"coverged":"not converged") << ", score: " << icp.getFitnessScore() << std::endl;
-    std::cout << icp.getFinalTransformation() << std::endl;
     
-    // Save the aligned point cloud as a .ply file
-    std::filesystem::create_directory("ICP_Aligned_PC");
-    pcl::io::savePLYFileBinary("ICP_Aligned_PC/aligned_frame.ply", *final_cloud);
-    std::cout << "Saved frame.\n";
+    float leafSize = std::stof(argv[2]);
+
+    std::string pcDir = argv[1];
+    std::string sourcePLYFile;
+    std::string targetPLYFile;
+
+    std::string outputDir = "Aligned ICP Point Clouds";
+    fs::create_directory(outputDir);
+
+    if (!(fs::is_directory(pcDir))) {
+        std::cout << std::string(pcDir) <<" is not a directory\n";
+        return 1;
+    }
+    
+    std::vector<std::string> frames;
+    
+    for (auto const& plyFile : fs::directory_iterator{pcDir}) {
+        if (plyFile.path().extension()==".ply") {
+            frames.push_back(plyFile.path());
+        }
+    }
+    std::sort (frames.begin(), frames.end());
+
+    for (auto i : frames) {
+        std::cout << i << "\n";
+    }
+    
+    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloudS (new pcl::PointCloud<pcl::PointXYZRGBNormal>());
+    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloudT (new pcl::PointCloud<pcl::PointXYZRGBNormal>());
+    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr alignedCloud (new pcl::PointCloud<pcl::PointXYZRGBNormal>());        
+    pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr GlobalCloud (new pcl::PointCloud<pcl::PointXYZRGBNormal>());
+
+    pcl::IterativeClosestPointWithNormals<pcl::PointXYZRGBNormal, pcl::PointXYZRGBNormal> icp;
+
+    Eigen::Matrix4f tGlobal = Eigen::Matrix4f::Identity();
+    
+    for (size_t i = 0; i<frames.size()-1; i++) {
+        std::cout << "This is the i-th frame: \t" << frames[i] << std::endl;
+        std::cout << "This is the [i+1]-th frame: \t" << frames[i+1] << std::endl;
+        std::cout << "===================================\n";
+        sourcePLYFile = frames[i];
+        targetPLYFile = frames[i+1];
+
+        std::cout << sourcePLYFile << std::endl;
+
+        if (pcl::io::loadPLYFile(sourcePLYFile, *cloudS) != 0) {
+            std::cerr << "Error Loading in: " << sourcePLYFile << "\n";
+            return 1;
+        }
+        if (pcl::io::loadPLYFile(targetPLYFile, *cloudT) != 0) {
+            std::cerr << "Error Loading in: " << targetPLYFile << "\n";
+            return 1;
+        }
+
+        icp.setInputSource(cloudS);
+        icp.setInputTarget(cloudT);
+        icp.setTransformationEpsilon(1e-8);
+        icp.setMaximumIterations(60);
+        //icp.setMaxCorrespondenceDistance(5*leafSize);
+        icp.setEuclideanFitnessEpsilon(1e-8);
+        icp.align(*alignedCloud);
+
+        if (icp.hasConverged()) {
+
+            std::cout << "ICP has converged for point cloud: " << i << " -> " << (i+1) << "\t| Score: " << icp.getFitnessScore() << "\n";
+            Eigen::Matrix4f tRel = icp.getFinalTransformation();
+            tGlobal = tRel * tGlobal;
+            pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr currCloud (new pcl::PointCloud<pcl::PointXYZRGBNormal>());
+            
+            pcl::transformPointCloud<pcl::PointXYZRGBNormal>(*cloudS, *currCloud, tGlobal, 1);
+
+            *GlobalCloud += *currCloud;
+
+            GlobalCloud = voxelDownsample<pcl::PointXYZRGBNormal>(GlobalCloud, leafSize);
+
+            std::ostringstream oss;
+            oss << outputDir << "/AlignedFrame" << std::setfill('0') << std::setw(5) << i << ".ply";
+            pcl::io::savePLYFileBinary(oss.str(), *alignedCloud);
+
+        }
+        else {
+            std::cout << "ICP has not converged!\n";
+        }
+        
+    }
+
+    std::ostringstream oss;
+    oss << outputDir << "/FinalRegistration.ply";
+    pcl::io::savePLYFileBinary(oss.str(), *GlobalCloud);
 
     return 0;
+
+    
 
 }
--- src/PointCloud2/src/point_cloud2_extract.cpp
+++ src/PointCloud2/src/point_cloud2_extract.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <iomanip>
 #include <filesystem>
 #include <rclcpp/serialization.hpp>
 #include <rclcpp/serialized_message.hpp>
@@ -11,6 +12,7 @@
 #include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/pcl_base.h>
+#include <pcl/features/normal_3d.h>
 
 
 template <typename Point>
@@ -47,15 +49,15 @@ typename pcl::PointCloud<Point>::Ptr z_crop(
 	}
 
 int main(int argc, char** argv){
-	if (argc < 3) {
-		std::cerr << "Missing Arguments!\n"; // Command line argument foramt: ./<executable> path/to/bag/folder <int meanSOR = 200>
+	if (argc < 2) {
+		std::cerr << "Missing Arguments!\n"; // Command line argument format: ./<executable> path/to/bag/folder
 			return 1;
 	}
 
 	// Initialize bag path, output folder and topic to observed.	
 	std::string bag_path = argv[1];
-	std::string output_folder = "Extracted_Point_Clouds";
-	std::string filtered_folder = "Filtered_Point_Clouds";
+	std::string output_folder = "Extracted_Point_Clouds_(" + bag_path + ")";
+	std::string filtered_folder = "Filtered_Point_Clouds_(" + bag_path + ")";
 	std::string topic = "/cloud";
 
 	std::filesystem::create_directory(output_folder);
@@ -80,26 +82,38 @@ int main(int argc, char** argv){
 			ser_data.deserialize_message(&ser_msg, msg.get());
 			
 			// Initialize an empty point cloud to store data from the ROS bag message
-			pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+			pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+			pcl::PointCloud<pcl::Normal>::Ptr normal(new pcl::PointCloud<pcl::Normal>);
+			pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_with_normal(new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+
 			pcl::fromROSMsg(*msg, *cloud);
 			
-			auto sor_cloud = sor_filter<pcl::PointXYZ>(cloud, mean, std);
-			auto filtered_cloud = z_crop<pcl::PointXYZ>(sor_cloud);
+			auto sor_cloud = sor_filter<pcl::PointXYZRGB>(cloud, mean, std);
+			auto filtered_cloud = z_crop<pcl::PointXYZRGB>(sor_cloud);
+			
+			auto normal_cloud = pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal>();
+			normal_cloud.setInputCloud(filtered_cloud);
+			auto tree = std::make_shared<pcl::search::KdTree<pcl::PointXYZRGB>>();
+			normal_cloud.setSearchMethod(tree);
+			normal_cloud.setKSearch(20);
+
+			normal_cloud.compute(*normal);
+			pcl::concatenateFields(*filtered_cloud, *normal, *cloud_with_normal);
 
 			// Writes and saves data to separate .ply files
 			std::ostringstream oss, oss_f;
 
-			oss << output_folder << "/frame" << frame_id << ".ply";
+			oss << output_folder << "/frame" << std::setfill('0') << std::setw(5) << frame_id << ".ply";
 			pcl::io::savePLYFileBinary(oss.str(), *cloud);
-			std::cout << "Saved " << oss.str() << " (" << (*cloud).size() << " points)\n";
+			//std::cout << "Saved " << oss.str() << " (" << (*cloud).size() << " points)\n";
 
-			oss_f << filtered_folder << "/frame" << frame_id++ << ".ply";
-			pcl::io::savePLYFileBinary(oss_f.str(), *filtered_cloud);
-			std::cout << "Saved " << oss_f.str() << " (" << (*filtered_cloud).size() << " points)\n";
+			oss_f << filtered_folder << "/frame" << std::setfill('0') << std::setw(5) << frame_id++ << ".ply";
+			pcl::io::savePLYFileBinary(oss_f.str(), *cloud_with_normal);
+			//std::cout << "Saved " << oss_f.str() << " (" << (*cloud_with_normal).size() << " points)\n";
 
 		}
 	}
-	std::cout << "Saved " << frame_id << "frames\n";
+	std::cout << "Saved " << frame_id << " frames\n";
 	return 0;
 
 }
